810480d7ffb297e3f536eb228efd4d2244724fcb
federico1018 <faldana4@misena.edu.co>
2023-10-13 18:39:32 -0500

    BBVA VENTAS HOME/NAVBAR

--- src/config/controllers/home.controllers.js
+++ src/config/controllers/home.controllers.js
@@ -1,3 +1,4 @@
+import '../js/componentes/datoBuscado.js'
 import Home from '@views/home.html'
 
 export default () => {
@@ -7,6 +8,39 @@ export default () => {
 
     /** FUNCIONES INTERNAS DEL ELEMENTO**/
 
+    const inputBusqueda = document.getElementById('busqueda');
+    const articulos = document.querySelectorAll('.header__listdropdown__checklists a');
+
+    document.getElementById('srcDatos').style.display = 'none';
+
+    inputBusqueda.addEventListener('input', function () {
+        const valorBusqueda = inputBusqueda.value.toLowerCase();
+
+        articulos.forEach(articulo => {
+            const nombre = articulo.dataset.nombre.toLowerCase();
+            if (nombre.includes(valorBusqueda)) {
+                articulo.style.display = 'block';
+            } else {
+                articulo.style.display = 'none';
+            }
+        });
+
+        const algunArticuloVisible = Array.from(articulos).some(articulo => articulo.style.display !== 'none');
+        const formulario = document.getElementById('srcDatos');
+
+        // Mostrar el formulario si hay resultados coincidentes, de lo contrario, ocultarlo
+        formulario.style.display = algunArticuloVisible ? 'block' : 'none';
+    });
+
+    // Manejar cuando el campo de búsqueda se borra
+    inputBusqueda.addEventListener('blur', function () {
+        if (inputBusqueda.value === '') {
+            articulos.forEach(articulo => {
+                articulo.style.display = 'none';
+            });
+            document.getElementById('srcDatos').style.display = 'none';
+        }
+    });
     /** **/
 
     return subdocument
--- src/config/js/componentes/InputList.js
+++ src/config/js/componentes/InputList.js
@@ -1,41 +1,41 @@
 class InputList extends HTMLElement {
-    constructor() {
-        super();
-        this.data = "data=Titulo";
-        this.number = "1";
+  constructor() {
+    super();
+    this.data = "data=Titulo";
+    this.number = "1";
+  }
+
+  static get observedAttributes() {
+    return ["data", "number", 'need', 'op', 'namepadre', 'nombre'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case "data":
+        this.data = newValue;
+        break;
+      case "number":
+        this.number = newValue;
+        break;
+      case 'op':
+        this.op = newValue
+        break;
+      case 'namepadre':
+        this.namepadre = newValue;
+        break;
+      case 'nombre':
+        this.nombre = newValue;
+        break;
+      case "need":
+        if (this.need == undefined) {
+          this.need = "need"
+        }
+        break;
     }
-  
-    static get observedAttributes() {
-      return ["data", "number", 'need', 'op', 'namepadre', 'nombre'];
-    }
-  
-    attributeChangedCallback(name, oldValue, newValue) {
-      switch (name) {
-        case "data":
-          this.data = newValue;
-          break;
-        case "number":
-          this.number = newValue;
-          break;
-        case 'op':
-          this.op = newValue
-          break;
-        case 'namepadre':
-          this.namepadre = newValue;
-          break;
-        case 'nombre':
-          this.nombre = newValue;
-          break;          
-        case "need":
-          if (this.need == undefined) {
-            this.need = "need"
-          }
-          break;
-      }
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
         <div class="chcontainer__boxdata">
             <label for="box${this.number}">${this.data}</label>
             <select 
@@ -51,7 +51,7 @@ class InputList extends HTMLElement {
     for (let i = 0; i < arrOptions.length; i++) {
       select.innerHTML += `<option value='${arrOptions[i]}'>${arrOptions[i]}</option>`
     }
-    }
+  }
 }
-  
+
 window.customElements.define("input-list", InputList);
