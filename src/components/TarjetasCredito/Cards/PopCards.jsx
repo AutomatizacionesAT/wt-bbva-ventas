178a49cb323a258e8a93b41d13e2aca74df71544
nedagosa-cod <nestorcode4004@gmail.com>
2024-07-10 11:42:23 -0500

    add images pop cards

---
+++ src/components/TarjetasCredito/Cards/PopCards.jsx
@@ -0,0 +1,15 @@
+import imgCardStandar from '../../../assets/images/creditCards/Imagen1.png'
+
+const PopCards = () => {
+	return (
+		<section className="popCards">
+			<article className="popCards__box">
+				<figure>
+					<img src={imgCardStandar} alt="imagen niño" />
+				</figure>
+			</article>
+		</section>
+	)
+}
+
+export default PopCards
