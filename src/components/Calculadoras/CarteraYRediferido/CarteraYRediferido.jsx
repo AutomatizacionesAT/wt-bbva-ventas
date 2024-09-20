69c0cd635c98c0682183539ea3b5ac3fd6b0b6b4
Jersson Arley Osma Cifuentes <jerssonarleyosma@gmail.com>
2024-09-20 01:43:39 -0500

    añado tooltip

--- src/components/Calculadoras/CarteraYRediferido/CarteraYRediferido.jsx
+++ src/components/Calculadoras/CarteraYRediferido/CarteraYRediferido.jsx
@@ -535,6 +535,14 @@ export const CarteraYRediferido = () => {
                     
 
                     <div className={`carterarediferido__input ${datos.gracia != undefined? 'active' : ''}`}>
+                        <div className="carterarediferido__faq">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                <path
+                                d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"
+                                ></path>
+                            </svg>
+                            <span className="carterarediferido__tooltip">Se aplica así: <br /> <br/> 1) Compra con diferido a 1 ó 2 cuotas y Todos los Avances: se le debe poner (0) <br /> <br /> 2) Compra de Cartera, Rediferidos y Compra con un diferido igual o superior a 3  meses: se le debe poner (1)</span>
+                        </div>
                         <select
                             onChange={(e) => handleInputFormat(e.target.value, 'select', 'gracia', 'graciaFormated')}
                             value={datos.graciaFormated || ""}
@@ -549,6 +557,14 @@ export const CarteraYRediferido = () => {
                     </div>
 
                     <div className={`carterarediferido__input ${datos.tipo != undefined? 'active' : ''}`}>
+                        <div className="carterarediferido__faq">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                <path
+                                d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"
+                                ></path>
+                            </svg>
+                            <span className="carterarediferido__tooltip"> - Facturación = Compras PO <br/> <br/> <br/> - Compras Cartera = Demás Transacciones</span>
+                        </div>
                         <select
                             onChange={(e) => handleInputFormat(e.target.value, 'select', 'tipo', 'tipoFormated')}
                             value={datos.tipoFormated || ""}
